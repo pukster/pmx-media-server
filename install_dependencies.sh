514b8e15a185287023b4d4cd388a72cefcd33d86
Peyman Askari <peyman.askari@prilyx.com>
2019-02-13 20:32:06 -0800

    Fixing script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get upgrade
+
 #installation Hyperion
-sudo apt-get install libqtcore4 libqtgui4 libqt4-network libusb-1.0-0 libprotobuf7 ca-certificates
-cd /tmp && wget -N https://raw.github.com/tvdzwan/hyperion/master/bin/install_hyperion.sh &amp;&amp; sudo sh ./install_hyperion.sh
+sudo apt-get install libqtcore4 libqtgui4 libqt4-network libusb-1.0-0 libprotobuf9 ca-certificates
+cd /tmp && wget -N https://raw.github.com/tvdzwan/hyperion/master/bin/install_hyperion.sh
+sudo sh ./install_hyperion.sh
 
 #copy config from sd card
 sudo cp hyperion.config.json /etc/hyperion.config.json
 
 #Hyperion restart + test
 sudo killall hyperiond
-/usr/bin/hyperiond /etc/hyperion.config.json &lt;/dev/null &gt;/dev/null 2&gt;&amp;1 &amp;
+/usr/bin/hyperiond /etc/hyperion.config.json < /dev/null >/dev/null 2>&1 &
 hyperion-remote --effect &quot;Rainbow swirl fast&quot; --duration 3000
